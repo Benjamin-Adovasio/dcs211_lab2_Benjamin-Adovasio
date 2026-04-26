e7b2d2add553e206fc234836070a84624784942e
Benjamin <benjamin@adovasio.com>
2026-04-26 11:53:45 -0400

    modified code to use cleaned data

--- dcs211_lab2.py
+++ dcs211_lab2.py
@@ -1,3 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from prettytable import PrettyTable
+
+arc_data = pd.read_csv(
+    "county_economic_status_2025.csv",
+    thousands=","
+)
+
+print(arc_data.head())
+print(arc_data.columns)

361ea3c757caa3e9dea612bdd1ebc174d598079f
Benjamin <benjamin@adovasio.com>
2026-04-26 11:57:02 -0400

    finished main part of lab 2

--- dcs211_lab2.py
+++ dcs211_lab2.py
@@ -10,3 +10,34 @@ arc_data = pd.read_csv(
 
 print(arc_data.head())
 print(arc_data.columns)
+
+# ✅ REMOVE EMPTY ROW
+arc_data = arc_data.iloc[1:]
+
+arc_data = arc_data.rename(columns={
+    "FIPS": "fips",
+    "State": "state",
+    "County": "county",
+    "Per Capita Market Income, 2022": "pci",
+    "Poverty Rate, 2018-2022": "poverty_rate",
+    "Three-Year Average Unemployment Rate, 2020-2022": "unemployment"
+})
+
+arc_data = arc_data[[
+    "fips",
+    "state",
+    "county",
+    "pci",
+    "poverty_rate",
+    "unemployment"
+]]
+
+arc_data["pci"] = pd.to_numeric(arc_data["pci"])
+arc_data["poverty_rate"] = pd.to_numeric(arc_data["poverty_rate"])
+arc_data["unemployment"] = pd.to_numeric(arc_data["unemployment"])
+
+print("\nCLEANED DATA:")
+print(arc_data.head())
+
+print("\nDATA TYPES:")
+print(arc_data.dtypes)
